53696d65f73f47d20eb98a702c7d4850166379fa
lamarrh <henry.773@osu.edu>
2018-09-10 21:39:59 -0400

    test

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=33507)
+    from os import environ
+    app.run(host='0.0.0.0' debug=False, port=environ.get("PORT", 5000))
